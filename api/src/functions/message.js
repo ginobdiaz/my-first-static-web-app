bb2c3012bdea0ea69fcdf42279104d39b07e2d13
Gino Diaz <Gino.Diaz@outlook.com>
2026-02-26 14:35:13 -0800

    Chg #36.4

--- api/src/functions/message.js
+++ api/src/functions/message.js
@@ -8,6 +8,6 @@ app.http('message', {
 
         const name = request.query.get('name') || await request.text() || 'world';
 
-        return { body: `Hello, ${name}!` };
+        return { 'body': `Hello, ${name}!` };
     }
 });
